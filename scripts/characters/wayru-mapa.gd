0f657ba72904990b47937eebef697e400e7673e5
yasuqm0 <yasuhirokawaiix80@gmail.com>
2025-06-15 01:07:50 -0500

    reparacion del movimiento del mapa
    modifique el script del mapa y ahora tiene limites, sin ningun bug

--- scripts/characters/wayru-mapa.gd
+++ scripts/characters/wayru-mapa.gd
@@ -1,27 +1,24 @@
 extends CharacterBody2D
 
-
-const velocidad = 50.0
-@onready var player = $".."
+const VELOCIDAD = 50.0
 @onready var animacion = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO  # Resetea la velocidad cada frame
+
 	if Input.is_action_pressed("arriba"):
-		player.position.y -= velocidad * delta
+		velocity.y = -VELOCIDAD
 		animacion.play("caminar-arriba")
-		
-	elif Input.is_action_pressed("izquierda"):
-		player.position.x -= velocidad * delta
-		animacion.play("caminar-izquierda")
-		
 	elif Input.is_action_pressed("abajo"):
-		player.position.y += velocidad * delta
+		velocity.y = VELOCIDAD
 		animacion.play("caminar-abajo")
-		
+	elif Input.is_action_pressed("izquierda"):
+		velocity.x = -VELOCIDAD
+		animacion.play("caminar-izquierda")
 	elif Input.is_action_pressed("derecha"):
-		player.position.x += velocidad * delta
+		velocity.x = VELOCIDAD
 		animacion.play("caminar-derecha")
-		
 	else:
 		animacion.stop()
+
 	move_and_slide()
